67f1fcad29b60b721553da9e4b6ebfb200049236
Yas Min <yasmin.lorenzo.c14@gmail.com>
2022-06-07 10:20:13 +0200

    wip(DB): populate posts table

--- database/populateDB.js
+++ database/populateDB.js
@@ -1,20 +1,22 @@
-require("dotenv").config();
-const bcrypt = require("bcrypt");
-const getPool = require("./getPool");
+import 'dotenv/config'
+import {getPool} from "./getPool.js";
 
 const populateDB = async () => {
     try {
         const pool = getPool();
-
-        console.log("Inserting data in users...");
-
         await pool.query(
-            `INSERT INTO users (avatar, name, email, password, role) VALUES 
+            `INSERT INTO posts (title, opening_line, text, topic, photo) VALUES
+            ("Nadal defies time", "Nadal seals dominant 6-3, 6-3, 6-0 win at Roland Garros",
+            "lorem ipsum", "sports", null)`
+        )
+      /*  console.log("Inserting data in users...");
+        //const bcrypt = require("bcrypt");
+        await pool.query(
+            `INSERT INTO users (avatar, name, email, password) VALUES 
       ("user@email.com", ?, "user", "user");`,
             [await bcrypt.hash("123456", 10)]
         );
-
-        console.log("¡All done!");
+*/
     } catch (error) {
         console.error(error);
     } finally {

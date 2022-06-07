e9c0637523b0eaf4469bbc6e6a8a00344f8bd174
Yas Min <yasmin.lorenzo.c14@gmail.com>
2022-06-07 10:48:50 +0200

    fix(DB): create DB getting DB name of .env

--- database/createDB.js
+++ database/createDB.js
@@ -4,7 +4,8 @@ import mysql from 'mysql2/promise'
 const {
     DATABASE_HOST,
     DATABASE_USER,
-    DATABASE_PASSWORD
+    DATABASE_PASSWORD,
+    DATABASE_NAME
 } = process.env
 
 const createDB = async () => {
@@ -14,9 +15,9 @@ const createDB = async () => {
         password: DATABASE_PASSWORD,
         connectionLimit: 10
     });
-    await pool.query("DROP DATABASE IF EXISTS project");
-    await pool.query("CREATE DATABASE project");
 
+    await pool.query(`DROP DATABASE IF EXISTS ${DATABASE_NAME}`)
+    await pool.query(`CREATE DATABASE ${DATABASE_NAME}`);
 };
 createDB();
 

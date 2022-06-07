b7b04fd9eb87240c8e504103c9d43684c8b5d7a7
Yas Min <yasmin.lorenzo.c14@gmail.com>
2022-06-07 10:57:24 +0200

    chore(server):switch to ES6 notation

--- server.js
+++ server.js
@@ -1,40 +1,47 @@
-require("dotenv").config();
-const express = require("express");
+import 'dotenv/config'
+import express from 'express'
 
 const { SERVER_PORT } = process.env;
+const app = express();
 
-/*const {
-    validateAuth,
-    checkAdmin,
-    notFound,
-    handleError,
-} = require("./middlewares");*/
+app.use(express.json());
 
 /*const {
     registerUser,
     activateUser,
     loginUser,
-    deleteUser,
-} = require("./controllers/users");*/
+} = require("./controllers/userController");*/
 
-/*const { createEntry, editEntry } = require("./controllers/entries");*/
-
-const app = express();
-
-app.use(express.json());
+/*const { getAllPosts, getPostById,createPost, editPost } = require("./controllers/postController");*/
 
 /*app.post("/users", registerUser);
 app.put("/users/activate/:registrationCode", activateUser);
 app.post("/login", loginUser);
 app.post("/entries", validateAuth, createEntry);
 app.patch("/entries/:idEntry", validateAuth, editEntry);
-app.delete("/users/:idUser", validateAuth, checkAdmin, deleteUser);*/
 
 /** Middleware 404 */
-app.use(notFound);
+app.use((request, response) => {
+    response.status(404).send({
+        status: "error",
+        message: "Not found",
+    });
+});
 
 /** Middleware error */
-app.use(handleError);
+app.use((error, request, response, next) => {
+    console.error(error);
+
+    resp.status(error.httpStatus || 500).send({
+        status: "error",
+        message: error.message,
+    });
+});
+/*const {
+    validateAuth,
+    notFound,
+    handleError,
+} = require("./middlewares");*/
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on http://localhost:${SERVER_PORT}`);

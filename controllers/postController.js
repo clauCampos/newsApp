d3e5b1a1d6abd0428296c8e7a3c3f4713a270d95
Yas Min <yasminlcg@gmail.com>
2022-06-15 10:09:57 +0200

    refactor: improve legibility and switch to throw error when status isn't ok

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,176 +1,126 @@
 import {
-  createPost,
-  deletePostById,
-  findPostByTopic,
-  getPosts,
-  collectLatestPosts,
-  findPostByDate,
-  findPostById,
-  selectPostById,
-  updatePostById
-} from "../repositories/postRepository.js";
+    createPost, deletePostById, findPostByTopic, getPosts, collectLatestPosts, findPostByDate, findPostById,
+    updatePostById} from "../repositories/postRepository.js";
 import {generateError} from "../helpers/generateError.js";
 import {createPostSchema, editPostSchema, idPostSchema} from "../schemas-validation/postSchema.js";
 
 const addPost = async (request, response, next) => {
-  try {
+    try {
+        await createPostSchema.validateAsync(request.body);
+        const actualDate = new Date(Date.now());
+        const user_id = request.auth.id;
+        const {title, opening_line, text, topic, photo} = request.body;
 
-    await createPostSchema.validateAsync(request.body);
-    const actualDate = new Date(Date.now());
-    const user_id = request.auth.id;
-    const { title, opening_line, text, topic, photo } = request.body;
-    const insertedId = await createPost(
-      title,
-      opening_line,
-      text,
-      topic,
-      photo,
-      actualDate,
-      user_id
-    );
-    response
-      .status(200)
-      .send({
-        status: "ok",
-        message: `new post created with id: ${insertedId}`,
-      });
-  } catch (error) {
-    next(error);
-  }
+        const insertedId = await createPost(title, opening_line, text, topic, photo, actualDate, user_id);
+        response.status(200)
+            .send({status: "ok", message: `new post created with id: ${insertedId}`});
+    } catch (error) {
+        next(error);
+    }
 };
 
 const getLatestPosts = async (request, response, next) => {
-  try {
-    const posts = await collectLatestPosts();
-    if (posts.length === 0) {
-      response
-        .status(404)
-        .send({ status: "error", message: "No posts exists" });
-    } else {
-      response.status(200).send({ status: "ok", data: posts });
+    try {
+        const posts = await collectLatestPosts();
+        if (posts.length === 0) {
+            throw generateError(`No post created in the last 24 hours.`, 404);
+        } else {
+            response.status(200).send({status: "ok", data: posts});
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 const getAllPosts = async (request, response, next) => {
-  try {
-    const posts = await getPosts();
-    if (posts.length === 0) {
-      response
-        .status(404)
-        .send({ status: "error", message: "No posts exists" });
-    } else {
-      response.status(200).send({ status: "ok", data: posts });
+    try {
+        const posts = await getPosts();
+        if (posts.length === 0) {
+            throw generateError(`No post exists.`, 404);
+        } else {
+            response.status(200).send({status: "ok", data: posts});
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 const getPostsByTopic = async (request, response, next) => {
-  try {
-    const { topic } = request.params;
-    const posts = await findPostByTopic(topic);
+    try {
+        const {topic} = request.params;
+        const posts = await findPostByTopic(topic);
 
-    if (posts.length === 0) {
-      response
-        .status(404)
-        .send({ status: "error", message: "No posts of that topic" });
-    } else {
-      response.status(200).send({ status: "ok", data: posts });
+        if (posts.length === 0) {
+            throw generateError(`No posts founded about that topic.`, 404);
+        } else {
+            response.status(200).send({status: "ok", data: posts});
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 const getPostsByDate = async (request, response, next) => {
-  try {
-    const { date } = request.params;
+    try {
+        const {date} = request.params;
 
-    const posts = await findPostByDate(date);
+        const posts = await findPostByDate(date);
 
-    if (posts.length === 0) {
-      response
-        .status(404)
-        .send({ status: "error", message: "No posts of that date" });
-    } else {
-      response.status(200).send({ status: "ok", data: posts });
+        if (posts.length === 0) {
+            throw generateError(`No posts created at the date: ${date}.`, 404);
+        } else {
+            response.status(200).send({status: "ok", data: posts});
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 const deletePost = async (request, response, next) => {
-  try {
-    const currentUserId = request.auth.id;
-    console.log(currentUserId);
-    const { idPost } = request.params;
-    const dataPost = await findPostById(idPost);
-    console.log(dataPost);
+    try {
+        const currentUserId = request.auth.id;
+        const {idPost} = request.params;
+        const dataPost = await findPostById(idPost);
 
-    if (!dataPost) {
-      response
-        .status(400)
-        .send({ status: "error", message: `id post= ${idPost} doesn't exist` });
-    } else if (dataPost.user_id !== currentUserId) {
-      response
-        .status(400)
-        .send({
-          status: "error",
-          message: `This post is not yours to delete it`,
-        });
-    } else {
-      const rowToDelete = await deletePostById(idPost);
-      if (rowToDelete === 0) {
-        response
-          .status(400)
-          .send({
-            status: "error",
-            message: `id post= ${idPost} doesn't exist`,
-          });
-      } else {
-        response
-          .status(200)
-          .send({ status: "ok", message: `deleted post number: ${idPost}` });
-      }
+        if (!dataPost) {
+            throw generateError(`id post: ${idPost} doesn't exist`, 404);
+        }
+        if (dataPost.user_id !== currentUserId) {
+            throw generateError("Deleting other users posts is not allowed!", 400);
+        }
+        const rowToDelete = await deletePostById(idPost);
+
+        if (rowToDelete === 0) {
+            throw generateError(`id post: ${idPost} doesn't exist`, 404);
+        } else {
+            response.status(200)
+                .send({status: "ok", message: `deleted post number: ${idPost}`});
+        }
+
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 const editPost = async (request, response, next) => {
     try {
-      const { idPost } = request.params;
-      await idPostSchema.validateAsync(idPost)
-      await editPostSchema.validateAsync(request.body);
-      const post = await selectPostById(idPost);
+        const {idPost} = request.params;
+        await idPostSchema.validateAsync(idPost)
+        await editPostSchema.validateAsync(request.body);
+        const post = await findPostById(idPost);
 
-      if (!post) {
-        throw generateError("Post does not exist!", 404);
-      }
-  
-      const userID = request.auth.id;
-      if (post.user_id !== userID) {
-        throw generateError("Editing other users posts is not allowed!", 400);
-      }
-  
-      await updatePostById({ ...post, ...request.body });
-  
-      response.status(200).send({ status: "ok", message: "Post updated!" });
+        if (!post) {
+            throw generateError("Post doesn't exist!", 404);
+        }
+
+        const userID = request.auth.id;
+        if (post.user_id !== userID) {
+            throw generateError("Editing other's users posts is not allowed!", 400);
+        }
+        await updatePostById({...post, ...request.body});
+        response.status(200).send({status: "ok", message: "Post updated!"});
+
     } catch (error) {
-      next(error);
+        next(error);
     }
-  };
-
-export {
-    addPost,
-    getLatestPosts,
-    getAllPosts,
-    getPostsByTopic,
-    getPostsByDate,
-    deletePost,
-    editPost
 };
+
+export {addPost, getLatestPosts, getAllPosts, getPostsByTopic, getPostsByDate, deletePost, editPost};
--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,12 +1,14 @@
 import {getPool} from "../database/getPool.js";
 
 const pool = getPool();
+
 const insertUser = async (nick, email, bio, avatar, encryptedPassword) => {
-    await pool.query(`INSERT INTO users (nick_name, email, password, bio, avatar) 
-                        VALUES (?,?,?,?,?)`, [nick, email, encryptedPassword, bio, avatar])
+    await pool.query(
+        `INSERT INTO users (nick_name, email, password, bio, avatar) 
+         VALUES (?,?,?,?,?)`, [nick, email, encryptedPassword, bio, avatar]);
 }
 const findUserByEmail = async (email) => {
-    const [user] = await pool.query(`SELECT * FROM project_2.users WHERE email= "${email}"`)
+    const [user] = await pool.query(`SELECT * FROM users WHERE email= "${email}"`)
     return user;
 
 }
